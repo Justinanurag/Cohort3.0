dc6c74758e320b0abcd9ee12f3fbbdf42eea430c
justinanurag <justinanurag0.2@gmail.com>
2025-06-16 12:34:09 +0530

    Self Practices

--- Week05/test/index.js
+++ Week05/test/index.js
@@ -13,7 +13,7 @@ app.post('/sum',(req,res)=>{
     const b=parseInt(req.body.b);
     const result=a+b;
     res.send({sum: result});
-     console.log(`Received numbers: ${a} + ${b}. Sent sum: ${result}`);
+     console.log(`The value of: ${a} + ${b}. Sent sum: ${result}`);
 });
 app.listen(3000,()=>{
     console.log('Server is running on port 3000');
