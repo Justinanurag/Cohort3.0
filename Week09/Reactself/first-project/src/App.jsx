8b316842bb25092385a35030437d9ac261ed39e9
justinanurag <justinanurag0.2@gmail.com>
2025-08-09 21:25:56 +0530

    week-9-2nd class complete(09-aug)

--- Week09/Reactself/first-project/src/App.jsx
+++ Week09/Reactself/first-project/src/App.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [count2, setCount2] = useState(0); 
+
+  function increase() {
+    setCount((count) => count + 1);
+  }
+  function decrease() {
+    setCount2((count2) => count2 + 1);
+  }
+
   return (
-    <div>App</div>
-  )
+    <div>
+      <Counter count={count} count2={count2} />
+      <button onClick={increase}>Increase Count</button>
+      <button onClick={decrease}>Decrease Count</button>
+    </div>
+  );
 }
 
-export default App
+function Counter(props) {
+  useEffect(function () {
+    console.log("render ");
+    return function () {
+      console.log("unmount");
+    };
+  }, []);
+
+  return (
+    <div>
+      Counter {props.count}
+      <br />
+      Counter2 {props.count2}
+    </div>
+  );
+}
+
+export default App;
