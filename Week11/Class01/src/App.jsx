fa227673e621f5f2eff2281ede20bb351c3a1546
justinanurag <justinanurag0.2@gmail.com>
2025-09-01 22:37:01 +0530

    learn about useFetch

--- Week11/Class01/src/App.jsx
+++ Week11/Class01/src/App.jsx
@@ -1,45 +1,15 @@
-import React, { useState } from 'react'
-//Custom hooks
-function useCounter(){
-  const [count,setCount]=useState(0);//Create State variable 
-  function increaseCount(){ //Define the function
-    setCount(count+1);
-  }
-  function decreseCounter(){
-    setCount(count-1);
-  }
- 
-  return {// return function as a state variable 
-    count:count,
-    increaseCount:increaseCount,
-    decreseCounter:decreseCounter
-  }
-}
+import React, { useEffect, useState } from 'react'
+import { useFetch, usePost } from './Hooks/useFetch'
 
 const App = () => {
-  const{count,increaseCount}=useCounter();
-  // const [count,setCount]=useState(0);
-  // function increaseCount(){
-  //   setCount(count=>count+1)
-  // }
+  const postData=usePost();
+const {finalData}=useFetch("https://jsonplaceholder.typicode.com/posts/1")
   return (
     <div>
-     <Counter/>
-     <CounterDec/>
+      {JSON.stringify(finalData)}
+    {/* {postData.title} */}
     </div>
   )
 }
 
-function Counter(){
-  const {count,increaseCount}=useCounter();
-  return <div>
-    <button onClick={increaseCount}>Increase {count}</button>
-  </div>
-}
-function CounterDec(){
-  const{count,decreseCounter}=useCounter();
-  return <div>
-    <button onClick={decreseCounter} >Decrease {count}</button>
-  </div>
-}
 export default App
