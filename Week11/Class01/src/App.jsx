27322bf8d3fe7139f234d502ff26b253ae315ae5
justinanurag <justinanurag0.2@gmail.com>
2025-09-05 16:15:28 +0530

    Week-11-Class 01started

--- Week11/Class01/src/App.jsx
+++ Week11/Class01/src/App.jsx
@@ -21,21 +21,58 @@
 
 //Learning about userPrev
 
-import React, { useState } from 'react'
-import { usePrev } from './Hooks/usePrev';
+// import React, { useState } from 'react'
+// import { usePrev } from './Hooks/usePrev';
 
+
+// const App = () => {
+//   const[count,setCount]=useState(0);
+//   const prev=usePrev(count);
+
+//   return (
+//     <div>
+//       <p>{count}</p>
+//       <button onClick={()=>{setCount(c=>c+1)}}>Click me</button>
+//       <p>The previous value was: {prev}</p>
+//     </div>
+//   )
+// }
+
+// export default App
+
+//Learning about useDebounce
+
+import React, { useRef } from 'react'  
+
+function useDebounce(callback, delay = 200) {
+  const currentClock = useRef();
+
+  const debouncedFn = (...args) => {
+    clearTimeout(currentClock.current);
+    currentClock.current = setTimeout(() => {
+      callback(...args);
+    }, delay);
+  };
+
+  return debouncedFn;
+}
 
 const App = () => {
-  const[count,setCount]=useState(0);
-  const prev=usePrev(count);
+  function sendDataToBackend(e) {
+    console.log("Sending to backend:", e.target.value);
+    fetch("https://api.com", {
+      method: "POST",
+      body: JSON.stringify({ value: e.target.value }),
+    });
+  }
+
+  const debounceFn = useDebounce(sendDataToBackend, 500);
 
   return (
     <div>
-      <p>{count}</p>
-      <button onClick={()=>{setCount(c=>c+1)}}>Click me</button>
-      <p>The previous value was: {prev}</p>
+      <input type="text" onChange={debounceFn} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
