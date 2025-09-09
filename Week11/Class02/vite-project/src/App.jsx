2d94c0cc4eda2724ce21f3af53554a01ea97720c
justinanurag <justinanurag0.2@gmail.com>
2025-09-09 17:36:06 +0530

    week-11 class-2 end (Selector and recoil)

--- Week11/Class02/vite-project/src/App.jsx
+++ Week11/Class02/vite-project/src/App.jsx
@@ -94,6 +94,8 @@
 // Using recoil atom 
  //Step 01:Wrap the app inside a RecoilRoot
  
+
+ /*
 import React from 'react'
 import { RecoilRoot, atom, useRecoilValue, useSetRecoilState } from 'recoil';
 import { counterAtom } from './CounterAtom/counter';
@@ -148,5 +150,56 @@ function Increase() {
     <button onClick={increase}>Increase</button>
   </div>
 }
+
+export default App
+*/
+
+//Selector
+
+import React, { useState } from 'react'
+import { counterAtom, evenSelector } from './CounterAtom/counter';
+import { RecoilRoot, useRecoilValue,useSetRecoilState } from 'recoil';
+
+const App = () => {
+  return (
+    <div>
+      <RecoilRoot>
+      <Button/>
+      <Counter/>
+      <IsEven/>
+      </RecoilRoot>
+    </div>
+  )
+}
+
+function Button(){
+  const setCount=useSetRecoilState(counterAtom)
+
+function decrease(){
+  setCount(c=>c-1);
+}
+function increase(){
+  setCount(c=>c+2)
+}
+  return <div>
+    <button onClick={increase}>Increase</button>
+    <button onClick={decrease}>Decrease</button>
+  </div>
+}
+
+function Counter(){
+  const count=useRecoilValue(counterAtom);
+  return <div>
+    {count}
+  </div>
+}
+
+function IsEven(){
+  const even=useRecoilValue(evenSelector);
+  return <div>
+    {even ?"Even":"Odd"}
+  </div>
+}
+
 
 export default App
