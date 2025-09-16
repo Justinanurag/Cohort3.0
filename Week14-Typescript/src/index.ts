e181b2f21a7c4c827ad0dda253456e55ef32ac5a
Anurag Tiwari <justinanurag0.2@gmail.com>
2025-09-16 18:22:30 +0530

    Week-14 class-2 tailwind (16-sep-2025)

--- Week14-Typescript/src/index.ts
+++ Week14-Typescript/src/index.ts
@@ -24,15 +24,66 @@ delatedCall(function(){
     console.log("hello")
 })*/
 
-//5 Object 
-function greet(user:{
+//5 Object in typescript
+/*function greet(user:{
     name:string,
     rollno:number
 }){
     console.log("Object Name"+user.name)
-    console.log("Object Role Number",user.rollno)
+    console.log("Object Roll Number",user.rollno)
 }
 greet({
     name:"Anurag",
     rollno:10
-})
+})*/
+
+//6 Interface in typescript 
+/*
+interface UserType{
+    firstName:string,
+    lastName:string,
+    rollNo:number
+}
+function greet(user:UserType){
+console.log("Object Name:"+user.firstName+" "+ user.lastName)
+console.log("Object Roll Number",user.rollNo)   
+}
+
+greet({
+    firstName:"Anurag",
+    lastName:"Kumar",
+    rollNo:10
+})*/
+
+//7 Type in typescript
+/*
+type Employee={
+    name:string,
+    startDate:Date;
+};
+
+type Manager={
+    name:string,
+    department:string;
+};
+
+type TeamLead=Employee & Manager;
+
+const TeamLead:TeamLead={
+    name:"Anurag",
+    startDate:new Date(),
+    department:"IT"
+}
+*/
+
+//Week 14 class-2
+
+function isEven(num:number):boolean{
+    if(num%2==0){
+        return true;
+    } else{
+        return false;
+    }
+}
+console.log(isEven(10));
+console.log(isEven(11));
