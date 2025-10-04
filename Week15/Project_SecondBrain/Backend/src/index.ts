c26f687f896d1dd378a458c863f427d8b4865452
Anurag Tiwari <justinanurag0.2@gmail.com>
2025-10-04 10:30:55 +0530

    feat: Update CORS origin and adjust VITE_BASEURL for production environment

--- Week15/Project_SecondBrain/Backend/src/index.ts
+++ Week15/Project_SecondBrain/Backend/src/index.ts
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT||3000;
 
 app.use(cors({
-  origin: "http://localhost:5173", 
+  origin: "https://secondbrain-sepia.vercel.app", 
   methods: ["GET", "POST", "PUT", "DELETE"], 
   credentials: true, 
 }));
