f317d1e23ef98e3200b27957399f45b0f1c56135
justinanurag <justinanurag0.2@gmail.com>
2025-11-06 23:09:37 +0530

    Week-20 class-01

---
+++ Week20-NextJs/class01/app/(auth)/signin/page.tsx
@@ -0,0 +1,9 @@
+export default function(){
+    return <div>
+        Signin
+        <div>
+            <input type="text" placeholder="Enter mail" id="" />
+            <input type="password" placeholder="Enter password" id="" />
+        </div>
+    </div>
+}
