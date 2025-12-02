f12ae9a2bf3faa849731444c38ba86081e380074
Turbobot <turbobot@vercel.com>
2025-12-02 10:55:32 +0530

    feat(create-turbo): apply official-starter transform

--- Week22-Excalidraw/packages/Db/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaPg } from "@prisma/adapter-pg";
-import { pool } from "./config/dbConnection";
-import { PrismaClient } from "./generated/prisma/client";
-
-const adapter = new PrismaPg(pool);
-
-export const prisma = new PrismaClient({
-  adapter,
-});
